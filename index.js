9bf1502a188b3e9eefc01b92b41cb77409fc8019
Naftali Kulik <nkulik1994@gmail.com>
2022-04-26 16:52:06 -0400

    commit tree 6fcf139a341bde2565eebe292c539fb5d56c7e64

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(cat) {
+    return cats.push(cat)
+}
+function destructivelyPrependCat(cat) {
+    return cats.unshift(cat)
+}
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+function appendCat(cat) {
+    return [...cats, cat]
+}
+function prependCat(cat) {
+    return [cat, ...cats]
+}
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1)
+}
+function removeFirstCat() {
+    return cats.slice(1)
+}
